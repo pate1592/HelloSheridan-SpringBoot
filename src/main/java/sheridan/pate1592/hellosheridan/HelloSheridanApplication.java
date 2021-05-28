1469207f391d4d25101f12c46e1130b01bd47927
Khushaliben Patel <pate1592@sheridancollege.ca>
2021-05-28 13:24:26 -0400

    project created

---
+++ src/main/java/sheridan/pate1592/hellosheridan/HelloSheridanApplication.java
@@ -0,0 +1,13 @@
+package sheridan.pate1592.hellosheridan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSheridanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSheridanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/pate1592/hellosheridan/HelloSheridanApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.pate1592.hellosheridan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSheridanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
