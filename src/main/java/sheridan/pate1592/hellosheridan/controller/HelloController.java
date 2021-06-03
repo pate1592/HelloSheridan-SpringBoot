f4e867c789cd42bbb25b5b88c556e2dceb459c38
Khushaliben Patel <pate1592@sheridancollege.ca>
2021-06-03 14:23:26 -0400

    added path variable

--- src/main/java/sheridan/pate1592/hellosheridan/controller/HelloController.java
+++ src/main/java/sheridan/pate1592/hellosheridan/controller/HelloController.java
@@ -1,13 +1,27 @@
 package sheridan.pate1592.hellosheridan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/Hello")
-    public String hello(){
+    public String hello1(@RequestParam(defaultValue="") String name, Model model){
+        if(name.isEmpty()){
+            name = "Sheridan";
+        }
+        model.addAttribute( "name" ,name);
+        return "Hello";
+    }
+
+    @GetMapping("/Hello/{name}")
+    public String hello2(@PathVariable String name, Model model){
+
+        model.addAttribute( "name" ,name);
         return "Hello";
     }
 }
